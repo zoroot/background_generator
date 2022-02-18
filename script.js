337da771c4cfd1247a1c4137ed30c4c9d0a5dbab
Zoroot <kynshastarzzz@gmail.com>
2022-02-18 13:59:22 +0100

    Projet BackGround degrade

---
+++ script.js
@@ -0,0 +1,28 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomColor() {
+	color1.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click",randomColor);
